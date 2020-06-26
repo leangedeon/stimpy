4ea7e400b929b3faa0beba1c59db962552302ecf
lgedeon <lgedeon@medallia.com>
2020-06-26 15:38:37 -0300

    Updates

---
+++ precache-manifest.110c73371eb9dc285308a6a462179a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3e142ebbe90ed5dbcba623d8269a21",
+    "url": "/stimpy/index.html"
+  },
+  {
+    "revision": "57d0de1a67e3ae7cb4e5",
+    "url": "/stimpy/static/css/main.534d6a81.chunk.css"
+  },
+  {
+    "revision": "34e4f42f1eb0f1e5085c",
+    "url": "/stimpy/static/js/2.4436f183.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stimpy/static/js/2.4436f183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d0de1a67e3ae7cb4e5",
+    "url": "/stimpy/static/js/main.135010b8.chunk.js"
+  },
+  {
+    "revision": "e710ba798da13934798b",
+    "url": "/stimpy/static/js/runtime-main.b9f5dab8.js"
+  }
+]);
